e1aae8779eda09bb2253a2690fd640dde1fd5346
Bradley Hilton <graywolf336@craftyn.com>
2019-04-16 21:07:17 -0500

    Remove online player information when players are kicked

--- src/main/java/me/lxct/bestviewdistance/event/OnQuit.java
+++ src/main/java/me/lxct/bestviewdistance/event/OnQuit.java
@@ -3,6 +3,7 @@ package me.lxct.bestviewdistance.event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import static me.lxct.bestviewdistance.functions.data.Variable.onlinePlayers;
@@ -10,7 +11,12 @@ import static me.lxct.bestviewdistance.functions.data.Variable.onlinePlayers;
 
 public class OnQuit implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
-    public static void onPlayerQuit(final PlayerQuitEvent e) {
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        onlinePlayers.remove(e.getPlayer());
+    }
+    
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerKicked(PlayerKickEvent e) {
         onlinePlayers.remove(e.getPlayer());
     }
 }
